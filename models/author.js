d65d9ce106866120e4719072a12ea4b9b90ed208
Sumit Maharjan <sumitjan2@gmail.com>
2023-07-09 20:21:46 +1000

    first commit from terminal to add code to the git

---
+++ models/author.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+    first_name: {type: String, required: true, maxLength: 100},
+    family_name: {type: String, required: true, maxLength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual("name").get(function () {
+    //  To avoid errors in cases where an author does not have either a family name or first name
+    // we want to make sure we handle the exception by returning an empty string for that case
+    let fullname = "";
+    if (this.first_name && this.family_name){
+        fullname = `${this.family_name}, ${this.first_name}`;
+    }
+    
+    return fullname;
+});
+
+AuthorSchema.virtual("url").get(function () {
+    // we don't use an arrow function as we'll need the this object
+    return `/catalog/author/${this.id}`;
+});
+
+module.exports = mongoose.model("Author", AuthorSchema);
